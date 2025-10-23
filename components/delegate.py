11d0f8df213b565927e7d62605d94b67be2106dd
vlam.dao <vlam.dao@gmail.com>
2025-10-24 01:28:23 +0700

    document delegate

--- components/delegate.py
+++ components/delegate.py
@@ -1,18 +1,40 @@
 from PyQt5.QtWidgets import QStyledItemDelegate
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import QModelIndex
+from PyQt5.QtWidgets import QStyleOptionViewItem
+from typing import Optional
 
 class FontDelegate(QStyledItemDelegate):
-    def __init__(self, font_family=None, font_size=None, parent=None):
+    """
+    A delegate to customize the font of items in a view.
+
+    This delegate allows setting a specific font family and/or size for
+    items in a QAbstractItemView (like QTableWidget or QListView).
+    It modifies the existing font of the item, preserving other
+    attributes like weight or style.
+    """
+    def __init__(self, font_family: Optional[str] = None, font_size: Optional[int] = None, parent=None):
+        """
+        Initializes the FontDelegate.
+
+        Args:
+            font_family (Optional[str]): The font family to apply. Defaults to None.
+            font_size (Optional[int]): The font size to apply. Defaults to None.
+            parent (QObject, optional): The parent object. Defaults to None.
+        """
         super().__init__(parent)
         self.font_family = font_family
         self.font_size = font_size
 
-    def initStyleOption(self, option, index):
-        super().initStyleOption(option, index)
-        font = option.font
-        if self.font_family is not None:
-            font.setFamily(self.font_family)
-        if self.font_size is not None:
-            font.setPointSize(self.font_size)
-        option.font = font
+    def initStyleOption(self, option: QStyleOptionViewItem, index: QModelIndex):
+        """
+        Initializes the style option for the item at the given index.
 
+        This method is called by the view to get the style options for painting
+        an item. We override it to apply our custom font settings.
+        """
+        super().initStyleOption(option, index)
+        if self.font_family is not None:
+            option.font.setFamily(self.font_family)
+        if self.font_size is not None:
+            option.font.setPointSize(self.font_size)
