a48202db1d70bad66a43a672304e00007765510c
vlam.dao <vlam.dao@gmail.com>
2025-10-26 02:13:14 +0700

    optimize segment controls

--- features/video_cutter/components/segments/segment_controls.py
+++ features/video_cutter/components/segments/segment_controls.py
@@ -1,40 +1,70 @@
 from PyQt5.QtWidgets import (QWidget, QHBoxLayout, QPushButton, QLabel, QSizePolicy)
-from PyQt5.QtCore import QTime
+from PyQt5.QtCore import pyqtSignal
 
 from helper import ms_to_time_str
 
 class SegmentControls(QWidget):
+    _DEFAULT_START_TEXT = "Start: --:--:--.---"
+    _DEFAULT_END_TEXT = "End: --:--:--.---"
+
+    # --- Public Signals ---
+    set_start_clicked = pyqtSignal()
+    set_end_clicked = pyqtSignal()
+    cut_clicked = pyqtSignal()
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+
+        # Internal widgets, prefixed with _
+        self._set_start_button: QPushButton
+        self._start_label: QLabel
+        self._set_end_button: QPushButton
+        self._end_label: QLabel
+        self._cut_button: QPushButton
+        
         self._setup_ui()
+        self._connect_signals()
 
     def _setup_ui(self):
+        """Initializes and lays out the UI components."""
+        self._create_widgets()
+        self._setup_layout()
+
+    def _create_widgets(self):
+        """Creates the individual widgets for the controls."""
+        self._set_start_button = QPushButton("Set Start")
+        self._set_end_button = QPushButton("Set End")
+        self._cut_button = QPushButton("Cut Segments")
+
+        self._start_label = QLabel(self._DEFAULT_START_TEXT)
+        self._start_label.setFixedWidth(120)
+        self._end_label = QLabel(self._DEFAULT_END_TEXT)
+        self._end_label.setFixedWidth(120)
+
+    def _setup_layout(self):
+        """Sets up the layout for the controls."""
         layout = QHBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
-
-        self.set_start_button = QPushButton("Set Start")
-        self.set_end_button = QPushButton("Set End")
-        self.cut_button = QPushButton("Cut Segments")
-
-        self.start_label = QLabel("Start: --:--:--.---")
-        self.start_label.setFixedWidth(120)
-        self.end_label = QLabel("End: --:--:--.---")
-        self.end_label.setFixedWidth(120)
-
-        layout.addWidget(self.set_start_button)
-        layout.addWidget(self.start_label)
-        layout.addWidget(self.set_end_button)
-        layout.addWidget(self.end_label)
+        layout.addWidget(self._set_start_button)
+        layout.addWidget(self._start_label)
+        layout.addWidget(self._set_end_button)
+        layout.addWidget(self._end_label)
         layout.addStretch()
-        layout.addWidget(self.cut_button)
+        layout.addWidget(self._cut_button)
+
+    def _connect_signals(self):
+        """Connects internal widget signals to the public signals of this class."""
+        self._set_start_button.clicked.connect(self.set_start_clicked)
+        self._set_end_button.clicked.connect(self.set_end_clicked)
+        self._cut_button.clicked.connect(self.cut_clicked)
 
     def update_start_label(self, ms):
-        self.start_label.setText(f"Start: {ms_to_time_str(ms)}")
+        self._start_label.setText(f"Start: {ms_to_time_str(ms)}")
 
     def update_end_label(self, ms):
-        self.end_label.setText(f"End: {ms_to_time_str(ms)}")
+        self._end_label.setText(f"End: {ms_to_time_str(ms)}")
 
     def reset_labels(self):
-        self.start_label.setText("Start: --:--:--.---")
-        self.end_label.setText("End: --:--:--.---")
+        self._start_label.setText(self._DEFAULT_START_TEXT)
+        self._end_label.setText(self._DEFAULT_END_TEXT)
