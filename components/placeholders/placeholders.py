434e5c81b2e3afcab32396e1ab60c7a22f8e8010
vlam.dao <vlam.dao@gmail.com>
2025-11-02 14:24:36 +0700

    add improve placeholders getters

--- components/placeholders/placeholders.py
+++ components/placeholders/placeholders.py
@@ -7,20 +7,25 @@ if TYPE_CHECKING:
 
 class Placeholders:
     def __init__(self):
-        self.INPUTFILE_FOLDER = "{inputfile_folder}"
-        self.INPUTFILE_NAME = "{inputfile_name}"
-        self.INPUTFILE_EXT = "{inputfile_ext}"
-        self.OUTPUT_FOLDER = "{output_folder}"
+        self._INPUTFILE_FOLDER = "{inputfile_folder}"
+        self._INPUTFILE_NAME = "{inputfile_name}"
+        self._INPUTFILE_EXT = "{inputfile_ext}"
+        self._OUTPUT_FOLDER = "{output_folder}"
 
-        self.GENERAL_PLACEHOLDERS = [
-            self.INPUTFILE_FOLDER,
-            self.INPUTFILE_NAME,
-            self.INPUTFILE_EXT,
-            self.OUTPUT_FOLDER,
-        ]
-
+    def get_INPUTFILE_FOLDER(self):
+        return self._INPUTFILE_FOLDER
+    
+    def get_INPUTFILE_NAME(self):
+        return self._INPUTFILE_NAME
+    
+    def get_INPUTFILE_EXT(self):
+        return self._INPUTFILE_EXT
+    
+    def get_OUTPUT_FOLDER(self):
+        return self._OUTPUT_FOLDER
+    
     def get_placeholders_list(self):
-        return self.GENERAL_PLACEHOLDERS
+        return [self._INPUTFILE_FOLDER, self._INPUTFILE_NAME, self._INPUTFILE_EXT, self._OUTPUT_FOLDER]
     
     def get_replacements(self, input_file:  tuple[int, str, str], output_path: 'OutputPath'):
         if not input_file:
@@ -31,10 +36,10 @@ class Placeholders:
         output_folder = output_path.get_completed_output_path(inputfile_folder)
 
         replacements = {
-            self.INPUTFILE_FOLDER: str(inputfile_folder),
-            self.INPUTFILE_NAME: str(inputfile_name),
-            self.INPUTFILE_EXT: inputfile_ext.lstrip('.'),
-            self.OUTPUT_FOLDER: str(output_folder),
+            self._INPUTFILE_FOLDER: str(inputfile_folder),
+            self._INPUTFILE_NAME: str(inputfile_name),
+            self._INPUTFILE_EXT: inputfile_ext.lstrip('.'),
+            self._OUTPUT_FOLDER: str(output_folder),
         }
         return replacements
 
