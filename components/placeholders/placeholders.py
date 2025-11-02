056f44b7556a5ff2507acb7665c336756af298c1
vlam.dao <vlam.dao@gmail.com>
2025-11-02 15:27:19 +0700

    change get replacement

--- components/placeholders/placeholders.py
+++ components/placeholders/placeholders.py
@@ -27,14 +27,14 @@ class Placeholders:
     def get_placeholders_list(self):
         return [self._INPUTFILE_FOLDER, self._INPUTFILE_NAME, self._INPUTFILE_EXT, self._OUTPUT_FOLDER]
     
-    def get_replacements(self, input_file:  tuple[int, str, str], output_path: 'OutputPath'):
+    def get_replacements(self, input_file: str, output_folder: str):
         if not input_file:
             return {}
 
-        _, inputfile, inputfile_folder = input_file
-        inputfile_name, inputfile_ext = os.path.splitext(inputfile)
-        output_folder = output_path.get_completed_output_path(inputfile_folder)
-
+        inputfile_folder = os.path.dirname(input_file)
+        filename = os.path.basename(input_file)
+        inputfile_name, inputfile_ext = os.path.splitext(filename)
+        
         replacements = {
             self._INPUTFILE_FOLDER: str(inputfile_folder),
             self._INPUTFILE_NAME: str(inputfile_name),
