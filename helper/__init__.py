55a1540ea13a41fbcfeb2fea52ddfa3147a5252e
vlam.dao <vlam.dao@gmail.com>
2025-11-02 20:04:46 +0700

    utils

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,8 +1,11 @@
-from .utils import resource_path, ms_to_time_str, time_str_to_ms
+from .utils import *
 from .delegate import FontDelegate
 
 __all__ = ['resource_path', 
            'FontDelegate',
            'time_str_to_ms',
-           'ms_to_time_str'
+           'ms_to_time_str',
+           'bold_green',
+           'bold_red',
+           'bold_yellow'
            ]
